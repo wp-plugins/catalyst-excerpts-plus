ea46d27aaa2bab827e0705cba1623260fb6d9fa3
surfjam <surfjam@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-07 11:21:10 +0000

    1.2.5 to tags folder
    git-svn-id: https://plugins.svn.wordpress.org/catalyst-excerpts-plus/tags/1.2.5@486097 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ scripts.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) { /* You can safely use $ in this code block to reference jQuery */
+
+
+	$(function(){
+		$("span.cep-help").hover(function(){
+			$("#cep-thumblink-help-box").slideToggle();		
+		});
+	});
+	
+});
